527e46e73238dc38fcc0257a93069bec2fe8d7ac
Joaquin Mendiburu <joaquinmendiburu@gmail.com>
2022-06-30 11:26:46 -0300

    Rename apiEndpoint for backendUrl in config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,5 +1,5 @@
 export const config = {
   env: process.env.REACT_APP_ENV_NAME,
-  apiEndpoint: process.env.REACT_APP_BACKEND_URL,
+  backendUrl: process.env.REACT_APP_BACKEND_URL,
   publicUrl: process.env.PUBLIC_URL,
 };
--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosInstance } from 'axios';
 import { config } from '../config/config';
 
-const instance: AxiosInstance = axios.create({ baseURL: config.apiEndpoint });
+const instance: AxiosInstance = axios.create({ baseURL: config.backendUrl });
 
 export default instance;
