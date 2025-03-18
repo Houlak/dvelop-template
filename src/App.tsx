4f88124e61999a3c73770bf4ea397f73afc4e9cf
Rodrigo Jurado <rodrigo.jurado16@gmail.com>
2025-03-18 16:05:02 -0300

    Remove duplicated router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import { BrowserRouter, RouterProvider } from 'react-router-dom';
+import { RouterProvider } from 'react-router-dom';
 import getRoutes from './routes/routes';
 import './styles/app.scss';
 
 function App() {
   return (
-    <BrowserRouter>
+    <>
       <RouterProvider router={getRoutes()} />
-    </BrowserRouter>
+    </>
   );
 }
 
